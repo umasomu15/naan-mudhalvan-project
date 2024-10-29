b0fd1573f3e3e36ce067ed858865ce9995eed18f
umasomu15 <umasomu15@gmail.com>
2024-10-29 10:58:16 +0530

    new change

--- src/compontes/Footer.jsx
+++ src/compontes/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return <footer className='py-5 bg-dark'>
-        <div className='container'><p className='m-0 text-center text-white'>Conditions of Use & Sale Privacy Notice Interest-Based Ads</p></div>
+        <div className='container'><p className='m-0 text-center text-white'>Your satifaction is our priority contact us for any assistances</p></div>
     </footer>
 }
 
--- src/compontes/Header.jsx
+++ src/compontes/Header.jsx
@@ -4,8 +4,8 @@ function Header() {
     return <header className="bg-dark py-5">
         <div className="container px-4 px-lg-5 my-5">
             <div className="text-center text-white">
-                <h1 className="display-4 fw-bolder">Shop in style</h1>
-                <p className="lead fw-normal text-white-50 mb-0">With this shop homepage template</p>
+                <h1 className="display-4 fw-bolder">Trendcart</h1>
+                <p className="lead fw-normal text-white-50 mb-0">Your Bag is ready-Finish your purchase!!..</p>
             </div>
         </div>
     </header>
